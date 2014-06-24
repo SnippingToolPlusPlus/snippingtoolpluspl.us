accc43e0d447b7afc15ae5a6837cfad260a74f5e
Talon Daniels <talondaniels@gmail.com>
2014-06-23 19:41:42 -0600

    Rename environment variable in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-DEPLOY_PATH='/var/www-test/'
+#WEBSITE_ROOT_PATH='/var/www-test/'
 currentuser=`whoami`
 
-echo "Deploying as user $currentuser" 
-rsync -rav src/ $DEPLOY_PATH
-chown -R www-data:adm $DEPLOY_PATH
-find $DEPLOY_PATH -type d -exec chmod 775 {} \;
-find $DEPLOY_PATH -type f -exec chmod 664 {} \;
+if [ -z "$WEBSITE_ROOT_PATH" ]; then
+	echo "Please set the WEBSITE_ROOT_PATH environment variable"
+	exit 1
+fi
+
+echo "Deploying as user $currentuser to $WEBSITE_ROOT_PATH"
+rsync -rav src/ $WEBSITE_ROOT_PATH
+chown -R www-data:adm $WEBSITE_ROOT_PATH
+find $WEBSITE_ROOT_PATH -type d -exec chmod 775 {} \;
+find $WEBSITE_ROOT_PATH -type f -exec chmod 664 {} \;
 
