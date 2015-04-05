92c1cb539a954e8880685917db7ca1aa28c91f7a
ShaneIsrael <shanemisrael@gmail.com>
2015-04-05 09:48:01 -0600

    Testing php

---
+++ src/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<title>Snipping Tool++</title>
+	<meta charset="utf-8">
+	<meta name="description" content="homepage for Snipping Tool++" />
+	<meta name="keywords" content="imgur, screenshot, snipping, tool, snippingtool"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+	<meta property="og:site_name" content="Snipping Tool++" />
+	<meta property="og:image" content="http://snippingtoolpluspl.us/media/facebook-share.png" />
+	<meta property="og:description" content="Snipping Tool++ is a dedicated imgur snippet/screenshot uploading tool with hotkey support, local image saving, and deletion link history!" />
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,600,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" media="screen" href="css/bootstrap.css"/>
+    <link rel="stylesheet" media="screen" href="css/bootstrap-responsive.css"/>
+    <link rel="stylesheet" media="screen" href="css/bootstrap-override.css"/>
+    <link rel="stylesheet" media="screen" href="css/style.css"/>
+	<link rel="stylesheet" media="screen" href="css/custom.css"/>
+	<link rel="stylesheet" href="css/rrssb.css" />
+
+ 	<script type="text/javascript">
+	(function() {var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+	s.src = document.location.protocol + '//wisepops.com/default/index/get-loader?user_id=9928';
+	var s2 = document.getElementsByTagName('script')[0];s2.parentNode.insertBefore(s, s2);})();
+    </script>
+
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-51934264-1', 'snippingtoolpluspl.us');
+	ga('send', 'pageview');
+	</script>
+
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script src="js/modernizr.js" type="text/javascript"></script>
+
+<script src="js/ddsmoothmenu.js" type="text/javascript" ></script> 
+<script src="js/selectnav.js" type="text/javascript" ></script> 
+<script src="js/custom.js" type="text/javascript" ></script>
+</head>
+<body>
+<div id="fb-root"></div>
+
+<script>
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=345754535502820&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- header starts
+================================================== -->
+<section id="header" class="clearfix">
+    <div class="container">
+        <div class="row"> 
+            <div class="span8 clearfix"> 
+                
+                <!--menu starts-->
+                <div id="smoothmenu" class="ddsmoothmenu">
+                    <ul id="nav">
+                        <li><a href="/" class="selected">Home</a></li>
+                        <li><a href="/features">Features</a> </li>
+                        <li><a href="/changelog">Changelog</a> </li>
+                        <li><a href="/donate" class="menu_color">Donate</a>
+                        <li><a href="/faq">FAQ</a> </li>
+                        </li>
+                    </ul>
+                </div>
+                <!--menu ends--> 
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!-- header ends
+================================================== --> 
+	<section id="content" class="clearfix"> 
+
+		<div class="container"> 
+			<div class="row">
+				<div class="span12">
+					<div class="logo-box"/>
+				</div>
+			</div>
+			<div class="spacer-40px"></div>
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     echo "<table><tr><th>ID</th><th>Name</th></tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td></tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+
+$conn->close();
+?>  
+			<div class="row">
+				<div class="span12">
+					<h1>Upload a region of your screen in three steps.</h1>
+					<ul class="list-3col text-center">
+						<li>
+							<div class="icon-large-bg"><img src="img/feature-icons/hotkey.png" alt="icon"></div>
+							<h2>Press hotkey</h2>
+							Press Ctrl-Shift-1 to begin taking a screenshot.
+						</li>
+						<li>
+							<div class="icon-large-bg"><img src="img/feature-icons/select.png" alt="icon"></div>
+							<h2>Select region</h2>
+							Select a region of your screen to capture.
+						</li>
+						<li>
+							<div class="icon-large-bg"><img src="img/feature-icons/cloud-upload.png" alt="icon"></div>
+							<h2>Upload</h2>
+							Press enter to upload. Link gets copied to your clipboard.
+						</li>
+					</ul>
+				</div>
+			</div>
+
+            <div class="row hide-on-mobile">
+                <div class="spacer-30px"></div>
+                <center>
+                    <iframe src="http://gfycat.com/ifr/BothSparklingAlleycat" frameborder="0" scrolling="no" width="600" height="252" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>
+                </center>
+                </div>
+			<div class="row">
+				<div class="span12">
+					<div class="dark-box">
+						<h1>Runs on <span class="color">Windows</span>.</h1>
+						<ul class="list-separator">
+							<li>Requires <a href="http://www.oracle.com/technetwork/java/javase/downloads/index.html">Java 7+</a></li>
+						</ul>
+                        <br>
+                        <div class="hide-on-mobile">
+                        <h2>Install via <a href="https://chocolatey.org/"><span class="color">Chocolatey!</span></a></h2>
+                        <center>
+                        <div class="nuget-badge">
+                            <p><code><span>C:\&gt; </span>choco install SnippingToolPlusPlus
+                            </code></p>
+
+                        </div>
+                        </center>
+                        <br>
+                        <h3>...or download it directly<span class="color"></span></h3>
+                        </div>
+                        <a href="https://github.com/SnippingToolPlusPlus/SnippingToolPlusPlus" class="mybtn">Source</a>
+						<a href="downloads/SnippingToolPlusPlus-6.3.3.zip" onclick="window.open('/changelog');ga('send', 'event', 'downloads', 'download', '6.3.3');" class="mybtn-large">Download</a>
+						<a href="/donate" class="mybtn-greenhov">Donate</a>
+						<div class="list-separator"></div>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="spacer-30px"></div>
+
+			<div class="row">
+				<div id="disqus_thread"></div>
+    				<script type="text/javascript">
+        			/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        			var disqus_shortname = 'snippingtoolplusplus'; // required: replace example with your forum shortname
+
+        			/* * * DON'T EDIT BELOW THIS LINE * * */
+        			(function() {
+            			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        			})();
+    				</script>
+    				<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+			</div>
+		</div>
+	</section>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+</body>
+
+	<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'snippingtoolplusplus'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    </script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery.1.10.2.min.js"><\/script>')</script>
+	<script src="js/rrssb.min.js"></script>
+</html>
